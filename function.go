05a887c0dd4afbad24a16d2a62095785b416dff6
Marco Siebecke <marco@siebecke.se>
2022-02-03 12:09:59 +0100

    initial import

---
+++ function.go
@@ -0,0 +1,42 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io"
+	"net/http"
+
+	"github.com/gentoomaniac/logging"
+
+	"github.com/rs/zerolog/log"
+)
+
+// HelloWorld prints the JSON encoded "message" field in the body
+// of the request or "Hello, World!" if there isn't one.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	logging.Setup(&logging.LoggingConfig{Json: true, Debug: true})
+
+	var d struct {
+		Message string `json:"message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		switch err {
+		case io.EOF:
+			fmt.Fprint(w, "Hello World!")
+			return
+		default:
+			log.Printf("json.NewDecoder: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+	}
+
+	if d.Message == "" {
+		fmt.Fprint(w, "Hello World!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/cloudfunction
+
+go 1.16
+
+require (
+	github.com/gentoomaniac/logging v0.0.2
+	github.com/rs/zerolog v1.26.1
+)
